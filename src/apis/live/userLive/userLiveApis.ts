55f3aa78203a4b73dc7b9e5de432c0d043158da8
Joseph Ijaola <ijabolu@gmail.com>
2025-04-23 09:12:27 +0100

    commit

--- src/apis/live/userLive/userLiveApis.ts
+++ src/apis/live/userLive/userLiveApis.ts
@@ -24,7 +24,7 @@ export class UserLiveApis extends AxiosGlobal{
 
     getUserById(id:any): AxiosPromise<Array<any>> {
         return this.axios.get(`${configs.context}/users/view/${id}`, {
-            headers: { "Content-Type": "aplication/json", "Accept": "aplication/json", "Authorization": `Bearer ${store.getState().data.login.value.token}`, "Access-Control-Allow-Origin": "*" },
+            headers: { "Content-Type": "application/json", "Accept": "application/json", "Authorization": `Bearer ${store.getState().data.login.value.token}`, "Access-Control-Allow-Origin": "*" },
         });
     }
 
@@ -44,55 +44,55 @@ export class UserLiveApis extends AxiosGlobal{
     
     logout(data:any): AxiosPromise<any> {
         return this.axios.post(`${configs.context}/logout`, data,{
-            headers: { "Content-Type": "aplication/json","Accept":"aplication/json","Authorization":`Bearer ${store.getState().data.login.value.token}`,"Access-Control-Allow-Origin":"*" },
+            headers: { "Content-Type": "application/json","Accept":"application/json","Authorization":`Bearer ${store.getState().data.login.value.token}`,"Access-Control-Allow-Origin":"*" },
           });
     }
     
     applyEduSilver(data:any): AxiosPromise<any> {
         return this.axios.post(`${configs.context}/edusilverplans/add`, data,{
-            headers: { "Content-Type": "aplication/json", "Accept":"aplication/json","Authorization":`Bearer ${store.getState().data.login.value.token}`,"Access-Control-Allow-Origin":"*" },
+            headers: { "Content-Type": "application/json", "Accept":"application/json","Authorization":`Bearer ${store.getState().data.login.value.token}`,"Access-Control-Allow-Origin":"*" },
           });
     }
 
     getAllEduSilver(): AxiosPromise<Array<any>> {
         return this.axios.get(`${configs.context}/edusilverplans`, {
-            headers: { "Content-Type": "aplication/json", "Accept": "aplication/json", "Authorization": `Bearer ${store.getState().data.login.value.token}`, "Access-Control-Allow-Origin": "*" },
+            headers: { "Content-Type": "application/json", "Accept": "application/json", "Authorization": `Bearer ${store.getState().data.login.value.token}`, "Access-Control-Allow-Origin": "*" },
         });
     }
 
     applyEduGold(data:any): AxiosPromise<any> {
         return this.axios.post(`${configs.context}/edugoldplans/add`, data,{
-            headers: { "Content-Type": "aplication/json", "Accept":"aplication/json","Authorization":`Bearer ${store.getState().data.login.value.token}`,"Access-Control-Allow-Origin":"*" },
+            headers: { "Content-Type": "application/json", "Accept":"application/json","Authorization":`Bearer ${store.getState().data.login.value.token}`,"Access-Control-Allow-Origin":"*" },
           });
     }
 
         getAllEduGold(): AxiosPromise<Array<any>> {
             return this.axios.get(`${configs.context}/edugoldplans`, {
-                headers: { "Content-Type": "aplication/json", "Accept": "aplication/json", "Authorization": `Bearer ${store.getState().data.login.value.token}`, "Access-Control-Allow-Origin": "*" },
+                headers: { "Content-Type": "application/json", "Accept": "application/json", "Authorization": `Bearer ${store.getState().data.login.value.token}`, "Access-Control-Allow-Origin": "*" },
             });
         }
 
         applyEduPremiumLoan(data:any): AxiosPromise<any> {
             return this.axios.post(`${configs.context}/edupremiumloanplans/add`, data,{
-                headers: { "Content-Type": "aplication/json", "Accept":"aplication/json","Authorization":`Bearer ${store.getState().data.login.value.token}`,"Access-Control-Allow-Origin":"*" },
+                headers: { "Content-Type": "application/json", "Accept":"application/json","Authorization":`Bearer ${store.getState().data.login.value.token}`,"Access-Control-Allow-Origin":"*" },
               });
         }
 
         getAllEduPremiumLoan(): AxiosPromise<Array<any>> {
             return this.axios.get(`${configs.context}/edupremiumloanplans`, {
-                headers: { "Content-Type": "aplication/json", "Accept": "aplication/json", "Authorization": `Bearer ${store.getState().data.login.value.token}`, "Access-Control-Allow-Origin": "*" },
+                headers: { "Content-Type": "application/json", "Accept": "application/json", "Authorization": `Bearer ${store.getState().data.login.value.token}`, "Access-Control-Allow-Origin": "*" },
             });
         }
 
         applyEduPremiumSchoolProcessing(data:any): AxiosPromise<any> {
             return this.axios.post(`${configs.context}/edupremiumschoolplans/add`, data,{
-                headers: { "Content-Type": "aplication/json", "Accept":"aplication/json","Authorization":`Bearer ${store.getState().data.login.value.token}`,"Access-Control-Allow-Origin":"*" },
+                headers: { "Content-Type": "application/json", "Accept":"application/json","Authorization":`Bearer ${store.getState().data.login.value.token}`,"Access-Control-Allow-Origin":"*" },
               });
         }
 
         getAllEduPremiumSchoolProcessing(): AxiosPromise<Array<any>> {
             return this.axios.get(`${configs.context}/edupremiumschoolplans`, {
-                headers: { "Content-Type": "aplication/json", "Accept": "aplication/json", "Authorization": `Bearer ${store.getState().data.login.value.token}`, "Access-Control-Allow-Origin": "*" },
+                headers: { "Content-Type": "application/json", "Accept": "application/json", "Authorization": `Bearer ${store.getState().data.login.value.token}`, "Access-Control-Allow-Origin": "*" },
             });
         }
 }
